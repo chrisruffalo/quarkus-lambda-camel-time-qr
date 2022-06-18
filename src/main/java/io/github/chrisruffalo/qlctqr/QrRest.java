8ab379f9f94dcdf0d257710531cb9cd5185c14f8
Chris Ruffalo <chris.ruffalo@gmail.com>
2022-06-18 22:26:02 +0000

    Rest service supports more options/formats Dev mode listens on all interfaces for use from a remote workstation/ssh Better output based on format from QrService

--- src/main/java/io/github/chrisruffalo/qlctqr/QrRest.java
+++ src/main/java/io/github/chrisruffalo/qlctqr/QrRest.java
@@ -2,6 +2,7 @@ package io.github.chrisruffalo.qlctqr;
 
 import io.quarkus.runtime.annotations.RegisterForReflection;
 import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.model.rest.RestParamType;
 
 import javax.enterprise.context.ApplicationScoped;
 
@@ -11,14 +12,35 @@ public class QrRest extends RouteBuilder {
 
     @Override
     public void configure() throws Exception {
-        rest("/time")
-            .produces("text/html")
-            .get()
+        rest("/")
+            .get("/time")
+                .param()
+                .name("invert")
+                .type(RestParamType.query)
+                .allowableValues("true", "false")
+                .defaultValue("false")
+                .endParam()
                 .param()
                 .name("format")
+                .type(RestParamType.query)
+                .allowableValues("html", "ascii", "png")
+                .defaultValue("png")
                 .endParam()
-            .to("direct:generate");
-
+                .to("direct:generate")
+            .get("/time.{format}")
+                .param()
+                .name("invert")
+                .type(RestParamType.query)
+                .allowableValues("true", "false")
+                .defaultValue("false")
+                .endParam()
+                .param()
+                .name("format")
+                .type(RestParamType.path)
+                .allowableValues("html", "ascii", "png")
+                .defaultValue("false")
+                .endParam()
+                .to("direct:generate");
     }
 
 }
--- src/main/java/io/github/chrisruffalo/qlctqr/QrService.java
+++ src/main/java/io/github/chrisruffalo/qlctqr/QrService.java
@@ -16,6 +16,7 @@ import javax.enterprise.context.ApplicationScoped;
 import javax.imageio.ImageIO;
 import javax.inject.Inject;
 import javax.inject.Named;
+import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.BufferedInputStream;
 import java.io.ByteArrayInputStream;
@@ -32,8 +33,6 @@ import java.util.stream.Collectors;
 @RegisterForReflection
 public class QrService extends RouteBuilder {
 
-    private static final boolean INVERT = false;
-
     @Inject
     Logger logger;
 
@@ -42,21 +41,58 @@ public class QrService extends RouteBuilder {
     private static final String LOWER_HALF = "▄";
     private static final String NONE = " ";
 
-    protected String charactersToEntity(final String input) {
-        return input.codePoints().mapToObj(codePoint -> String.format("&#%d;", codePoint)).collect(Collectors.joining(""));
-    }
-
     @Override
     public void configure() throws Exception {
         from("direct:generate")
             .to("bean:qr?method=generateQrCode")
-            .to("bean:qr?method=turnToAscii")
-            .to("bean:qr?method=html")
+            .choice().when(this.header("format").in("html", "ascii")) // both html and ascii rely on ascii first
+                .to("bean:qr?method=turnToAscii")
+                .otherwise()
+                    .setHeader("Content-Disposition", this.constant("inline"))
+                .end()
+                .choice().when(this.header("format").isEqualTo("html")) // html needs to get converted to entities and shoved in a html string
+                    .to("bean:qr?method=html")
+                .end()
+            .end()
             ;
+    }
+
+    // originally from http://www.java2s.com/example/java/2d-graphics/invert-black-and-white-bufferedimage.html
+    public static void invertBlackAndWhite(BufferedImage image) {
+        for (int i = 0; i < image.getWidth(); i++) {
+            for (int j = 0; j < image.getHeight(); j++) {
+                Color c = new Color(image.getRGB(i, j));
+                //invert white into black
+                if (c.equals(Color.white)) {
+                    image.setRGB(i, j, Color.black.getRGB());
+                }
+                //invert black into white
+                else if (c.equals(Color.black)) {
+                    image.setRGB(i, j, Color.white.getRGB());
+                }
+            }
+        }
+    }
+
+    protected static String charactersToEntity(final String input) {
+        return input.codePoints().mapToObj(codePoint -> String.format("&#%d;", codePoint)).collect(Collectors.joining(""));
     }
 
     public void generateQrCode(Exchange exchange) {
         final Message requestMessage = exchange.getIn();
+
+        int width = 200;
+        int height = 200;
+        if ("html".equalsIgnoreCase(requestMessage.getHeader("format", String.class))) {
+            width = 100;
+            height = 100;
+        } else if ("ascii".equalsIgnoreCase(requestMessage.getHeader("format", String.class))) {
+            width = 60;
+            height = 60;
+        }
+
+        boolean invert = "true".equalsIgnoreCase(requestMessage.getHeader("invert", String.class));
+
         final QRCodeWriter writer = new QRCodeWriter();
         try (final ByteArrayOutputStream baos = new ByteArrayOutputStream()){
             Object body = requestMessage.getBody();
@@ -64,13 +100,21 @@ public class QrService extends RouteBuilder {
                 body = ZonedDateTime.now().toString();
             }
 
-            final BitMatrix matrix = writer.encode(Objects.toString(body), BarcodeFormat.QR_CODE, 60, 60);
+            final BitMatrix matrix = writer.encode(Objects.toString(body), BarcodeFormat.QR_CODE, width, height);
             final MatrixToImageConfig config = new MatrixToImageConfig();
             final BufferedImage image = MatrixToImageWriter.toBufferedImage(matrix, config);
+
+            if (invert) {
+                invertBlackAndWhite(image);
+            }
+
+            // write after inverting
             ImageIO.write(image, "png", baos);
 
-            requestMessage.setHeader(Exchange.FILE_NAME, requestMessage.getMessageId() + ".png");
-            requestMessage.setBody(new BufferedInputStream(new ByteArrayInputStream(baos.toByteArray())));
+            byte[] bytes = baos.toByteArray();
+            requestMessage.setHeader("Content-Length", bytes.length);
+            requestMessage.setHeader("Content-Type", "image/x-png");
+            requestMessage.setBody(new BufferedInputStream(new ByteArrayInputStream(bytes)));
         } catch (WriterException | IOException e) {
             logger.error("Could not create QR code", e);
             requestMessage.setBody("{}");
@@ -81,7 +125,10 @@ public class QrService extends RouteBuilder {
     public void turnToAscii(Exchange exchange) {
         final Message in = exchange.getIn();
         StringBuilder sb = new StringBuilder();
-        if (in.getBody() instanceof InputStream iostream) {
+
+        boolean invert = "true".equalsIgnoreCase(in.getHeader("invert", String.class));
+
+        if (in.getBody() instanceof final InputStream iostream) {
             try {
                 final BufferedImage readImage = ImageIO.read(iostream);
                 int width = readImage.getWidth();
@@ -89,24 +136,29 @@ public class QrService extends RouteBuilder {
                 int minX = 0;
                 int maxX = width;
 
+                boolean doubledY = false;
                 boolean inContent = false;
-                for (int y = 0; y < height; y += 2) {
+                for (int y = 1; y < height; y += 2) {
                     final StringBuilder line = new StringBuilder();
                     for (int x = minX; x < maxX; x ++) {
-                        boolean upper = readImage.getRGB(x, y) == -16777216;
-                        boolean lower = readImage.getRGB(x, y+1) == -16777216;
-                        if (!inContent && (upper || lower)){
-                            minX = x - 3;
-                            maxX = width - x + 2;
-                            height = height - y + 2;
-                            inContent = true;
-                            line.append(String.join("", Collections.nCopies(maxX - minX,INVERT ? FULL : NONE))).append(System.lineSeparator());
-                            line.append(String.join("", Collections.nCopies(maxX - minX,INVERT ? FULL : NONE))).append(System.lineSeparator());
-                            line.append(INVERT ? FULL + FULL + FULL : NONE + NONE + NONE);
+                        final Color compare = Color.black;
+                        boolean upper = false;
+                        boolean lower = false;
+                        try {
+                            upper = compare.equals(new Color(readImage.getRGB(x, y )));
+                            lower = compare.equals(new Color(readImage.getRGB(x, y + 1)));
+                        } catch (ArrayIndexOutOfBoundsException ibex) {
+                            logger.errorf("index out of bounds at h=%d, w=%d", y, x);
+                            continue;
                         }
-                        if (INVERT) {
-                            upper = !upper;
-                            lower = !lower;
+                        if (!inContent && ((!invert && (upper || lower)) || (invert && (!upper || !lower)))){
+                            int spacing = 2;
+                            minX = x - spacing;
+                            maxX = width - x + (readImage.getHeight() / 30 > 2 ? 2 : 1); // hand tuning nonsense
+                            height = height - y + (invert ? 1 : 0);
+                            inContent = true;
+                            line.append(String.join("", Collections.nCopies(maxX - minX,invert ? FULL : NONE))).append(System.lineSeparator());
+                            line.append(String.join("", Collections.nCopies(spacing, invert ? FULL : NONE)));
                         }
                         if (inContent) {
                             String place = NONE;
@@ -124,22 +176,28 @@ public class QrService extends RouteBuilder {
                         sb.append(line);
                         sb.append(System.lineSeparator());
                     }
-                }
-                // it seems that there is always a half-line difference so we are compensating for this seeming half-line off-by-one error in the most obvious way
-                if (INVERT) {
-                    sb.append(String.join("", Collections.nCopies(maxX - minX, INVERT ? UPPER_HALF : LOWER_HALF))).append(System.lineSeparator());
+                    // repeat one line to make the oddly shaped ascii line up right
+                    if (!doubledY && y + 1 > height / 2) {
+                        y -= readImage.getHeight() / 30 > 2 ? 2 : 1; // hand tuning doubling
+                        doubledY = true;
+                    }
                 }
             } catch (IOException ex) {
                 logger.error("Could not read image from inputstream", ex);
             }
         }
+        in.setHeader("Content-Type", "text/plain");
+        in.setHeader("Content-Length", sb.toString().getBytes().length);
         in.setBody(sb.toString());
         exchange.setMessage(in);
     }
 
     public void html(final Exchange exchange) {
         final Message message = exchange.getIn();
-        message.setBody(String.format("<html lang='en'><head></head><body><pre><span style=\"font-family: monospace; font-size: 0.70em;\">%s</span></pre></body></html>", charactersToEntity(message.getBody(String.class))));
+        final String outputHtml = String.format("<html lang='en'><head></head><body><pre><span style=\"font-family: monospace; font-size: 0.5em;\">%s</span></pre></body></html>", charactersToEntity(message.getBody(String.class)));
+        message.setHeader("Content-Length", outputHtml.getBytes().length);
+        message.setHeader("Content-Type", "text/html");
+        message.setBody(outputHtml);
         exchange.setMessage(message);
     }
 
